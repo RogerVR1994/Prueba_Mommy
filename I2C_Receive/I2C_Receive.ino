b45868cdde493ff6c2545416d09135c90274f49e
Rogelio Ventura <rogeliovr94@gmail.com>
2017-08-20 18:38:34 -0500

    Prueba conexi[on

--- I2C_Receive/I2C_Receive.ino
+++ I2C_Receive/I2C_Receive.ino
@@ -3,7 +3,6 @@
 void setup() {
   Wire.begin(8);                
   Wire.onReceive(receiveEvent); 
-  Wire.onRequest(requestEvent); // register event
   Serial.begin(9600);          
 }
 
@@ -18,9 +17,4 @@ void receiveEvent(int howMany) {
   }
   int x = Wire.read();    
   Serial.println(x);         
-}
-
-void requestEvent() {
-  Wire.write("hola "); // respond with message of 6 bytes
-  // as expected by master
 }
