e7351ae372b7b80427dfa0bdb9f9456d2dc9348b
Rogelio Ventura <rogeliovr94@gmail.com>
2017-08-20 03:25:28 -0500

    Prueba de Master/Esclavo I2C

--- Luzi-Slave/Luzi-Slave.ino
+++ Luzi-Slave/Luzi-Slave.ino
@@ -89,8 +89,9 @@ void requestEvent() {
   char data[4];
   dato.toCharArray(data, 6);
   Serial.println(data);
-  Wire.write(dato); // respond with message of 6 bytes
+  Wire.write(data); // respond with message of 6 bytes
   // as expected by master
+  data=""; 
 }
 
 void receiveEvent(int howmany){
