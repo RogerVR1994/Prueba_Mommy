34de3dfda40ddc3ba2765cb94b4e129d78f1dead
Rogelio Ventura <rogeliovr94@gmail.com>
2017-08-20 04:11:51 -0500

    Prueba de Master/Esclavo I2C

--- Luzi-Slave/Luzi-Slave.ino
+++ Luzi-Slave/Luzi-Slave.ino
@@ -47,20 +47,7 @@ void loop(){
         dato= String(real_temp);
         break;
       case 2:
-        Serial.println("Tomar presion arterial");
-        for (int i = 0 ; i<10; i++){
-          map_pulso=map(analogRead(A2), 0, 1024, 100, 50);
-          z+=map_pulso;
-          delay(100);
-        }
-        pulso = z/10;
-        Serial.println(pulso);
-        dato=String(pulso);
-        Serial.println("hola");
-        Serial.println(dato);
-        z=0;
         
-        receive_data=0;
         break;
       case 3:
         Serial.println("Tomar glucosa");
@@ -90,6 +77,20 @@ void requestEvent() {
   Serial.println(data);
   if (receive_data==2){
     Serial.println("Boton 2");
+    Serial.println("Tomar presion arterial");
+        for (int i = 0 ; i<10; i++){
+          map_pulso=map(analogRead(A2), 0, 1024, 100, 50);
+          z+=map_pulso;
+          delay(100);
+        }
+        pulso = z/10;
+        Serial.println(pulso);
+        dato=String(pulso);
+        Serial.println("hola");
+        Serial.println(dato);
+        z=0;
+        
+        receive_data=0;
   }
   Wire.write(data); // respond with message of 6 bytes
   // as expected by master
